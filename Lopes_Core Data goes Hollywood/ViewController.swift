5c09ef0820fc4e1df72a81bb296cc7539d47eef0
Student <s525893@nw32044.nwmissouri.edu>
2016-11-11 16:01:22 -0600

    Initial Commit

---
+++ Lopes_Core Data goes Hollywood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lopes_Core Data goes Hollywood
+//
+//  Created by Lopes,Grevil Gonsalo on 11/11/16.
+//  Copyright © 2016 Lopes,Grevil Gonsalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
